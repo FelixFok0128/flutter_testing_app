df5ee197598e2e82c221661b87271ee4fd39cfb2
felix.fok <felix.fok@dreamshub.com>
2022-11-25 12:46:43 +0800

    [Update] updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:date_picker_timeline/extra/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_testing_app/carousel_slider.dart';
 import 'package:flutter_testing_app/chat.dart';
@@ -10,11 +9,18 @@ import 'Map/map.dart';
 import 'hyperLink.dart';
 
 import 'package:testing_package/testing_package.dart';
-// import 'package:testing_package/test_widget.dart';
+import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
-// import 'package:testing_package/test_function.dart';
+// import 'dart:io' as io;
+
 void main() {
   runApp(MyAppScreen());
+  logger.w("PrettyPrinter warning message");
+  logger.e("PrettyPrinter error message");
+  logger.d("PrettyPrinter debug message");
+  logger.i("PrettyPrinter info message");
+  // demo();
 }
 
 class MyAppScreen extends StatelessWidget {
@@ -32,12 +38,50 @@ class MyAppScreen extends StatelessWidget {
         '/map': (context) => CustomMarkerPage(),
         '/chat': (context) => ChatScreen(),
       },
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+var logger = Logger(
+    printer: PrettyPrinter(
+        methodCount: 0,
+        // errorMethodCount: 5,
+        lineLength: 50,
+        colors: true,
+        printEmojis: true,
+        printTime: true));
+
+var loggerNoStack = Logger(
+  printer: PrettyPrinter(methodCount: 0),
+);
+void demo() {
+  logger.d('Log message with 2 methods');
+
+  loggerNoStack.i('Info message');
+
+  loggerNoStack.w('Just a warning!', "Waring");
+
+  logger.e('Error! Something bad happened', 'Test Error');
+
+  loggerNoStack.v({'key': 5, 'value': 'something'});
+
+  Logger(printer: SimplePrinter(colors: true)).v('boom');
+}
+
+// class AppStateNotifier extends ChangeNotifier {
+//   //
+//   bool isDarkMode = false;
+
+//   void updateTheme(bool isDarkMode) {
+//     this.isDarkMode = isDarkMode;
+//     notifyListeners();
+//   }
+// }
+
+class FirstScreen extends StatefulWidget {
   // FIXME: You need to pass in your access token via the command line argument
   // --dart-define=ACCESS_TOKEN=ADD_YOUR_TOKEN_HERE
   // It is also possible to pass it in while running the app via an IDE by
@@ -50,6 +94,12 @@ class FirstScreen extends StatelessWidget {
       "pk.eyJ1IjoiZmVsaXhmb2tkaCIsImEiOiJjbDhsZ29vZDIwcWZhM29sNXc3OW11aTZvIn0.IacBVZC9djerJ8CeCZanbQ";
 
   const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +117,7 @@ class FirstScreen extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            getLink('/map', "map"),
+            getLink('/map2', "map2"),
             SizedBox(
               height: 20,
             ),
@@ -82,7 +132,24 @@ class FirstScreen extends StatelessWidget {
                   print(aS);
                   // testFunction func = testFunction();
                   // testFunction.returnAString;
-                })
+                }),
+            SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+                onPressed: () {
+                  Get.changeThemeMode(
+                      Get.isDarkMode ? ThemeMode.light : ThemeMode.dark);
+                },
+                child: Text.rich(
+                  TextSpan(children: [
+                    TextSpan(text: "Home: "),
+                    TextSpan(
+                        text: "https://flutterchina.club",
+                        style: TextStyle(color: Colors.blue),
+                        recognizer: null),
+                  ]),
+                )),
           ],
         ),
       ),
